e78cc99538b050387bedc9a5910906fe6bc09321
unBro2786 <seccion891@gmail.com>
2025-04-11 17:02:49 -0600

    Soporte para mostrar diferentes categorias a travez de un dropdown menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-const main = document.getElementsByTagName("main>div").item(0);
-const contenedorProductos = document.getElementById("contenedor-productos");
+const main = document.getElementsByTagName("main").item(0);
+const mainProds = document.getElementById("contenedor-productos");
+
+const ulMenu = document.getElementById("ulMenu");
 
 const URLMain = "https://fakestoreapi.com/products/";
+const URLCategories = "https://fakestoreapi.com/products/categories/";
 let res;
 
-function getData() {
-    fetch(URLMain)
+function getData(cat) {
+    fetch(URLMain + cat)
         .then((respose) => {
             respose.json().then((res) => {
                 console.log(res.length);
@@ -22,18 +25,80 @@ function getData() {
         });
 }
 
+function getCategories() {
+    const options = { "method": "GET" };
+    fetch(URLMain+"categories/", options)
+        .then((respose) => {
+            respose.json().then((res) => {
+                res.forEach((cat) => {
+                    ulMenu.insertAdjacentHTML("afterbegin",
+                        `
+                        <li>
+                            <a class = "dropdown-item" onclick = "getData('category/${cat}');" href = "#">
+                                ${cat}
+                            </a>
+                        </li>
+                        `
+                    );
+                })
+            });
+            console.log(respose);
+        })
+        .catch((err) => {
+            main.insertAdjacentHTML("afterbegin",
+                `<div> class = "alert alert-danger" role = "alert">
+            ${err.message}
+            </div>`
+            );
+        });
+}
+
 function createCards(prods) {
+    
+    mainProds.innerHTML="";
+    
     prods.forEach((res) => {
-        contenedorProductos.insertAdjacentHTML("beforeend",
-            `<div class="card" style="width: 18rem;">
+        mainProds.insertAdjacentHTML("beforeend",
+            `
+            <div class="card" style="width: 18rem;">
                 <img class="card-img-top" src="${res.image}" alt="${res.title}">
                 <div class="card-body">
                     <h5 class="card-title">${res.title}</h5>
                     <p class="card-text">${res.description.slice(0, 100)}...</a>
+                    <p></p>
+
+                    <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#${res.id}Modal">
+                            Launch demo modal
+                        </button>
                 </div>
-            </div>`
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="${res.id}Modal" tabindex="-1" role="dialog" aria-labelledby="${res.id}ModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="${res.id}ModalLabel">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            ${res.description}
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            `
         );
     });
 }
 
-getData();
+getData("");
+getCategories();
